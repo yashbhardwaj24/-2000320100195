4d082917cccaeb74fedcb8a8d2a1e03a6e7df26e
yashbhardwaj24 <94592850+yashbhardwaj24@users.noreply.github.com>
2023-07-22 11:49:18 +0530

    assigment 2

--- folder2/src/components/AllTrainsPage.jsx
+++ folder2/src/components/AllTrainsPage.jsx
@@ -1,30 +1,132 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-import TrainCard from "../components/TrainCard";
+import React, { useState } from "react";
 
-const AllTrainsPage = () => {
-  const [trains, setTrains] = useState([]);
+const TrainForm = () => {
+  const [formData, setFormData] = useState({
+    companyName: "",
+    ownerName: "",
+    rollNo: "",
+    ownerEmail: "",
+    accessCode: "",
+  });
 
-  useEffect(() => {
-    let url = ""
-    axios
-      .get("YOUR_API_ENDPOINT")
-      .then((response) => {
-        setTrains(response.data);
-      })
-      .catch((error) => {
-        console.error(error);
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const fetchData = async () => {
+    try {
+      let url = `http://20.244.56.144/train/register`;
+      const response = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
       });
-  }, []);
+      let data = await response.json();
+      if (!data) {
+        fetchData1();
+        alert("data inserted");
+      }
+    } catch (error) {
+      console.log("error 🐹 :" + error);
+    }
+  };
+
+  const fetchData1 = async () => {
+    try {
+      let url = `http://20.244.56.144/train/auth`;
+      const response = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          ...formData,
+          ownerName: "Ram",
+          ownerEmail: "ram@abc.edu",
+          rollNo: "1",
+        }),
+      });
+      let data = await response.json();
+      localStorage.setItem('Token', data)
+    } catch (error) {
+      console.log("error 🐹 :" + error);
+    }
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    fetchData();
+  };
 
   return (
-    <div>
-      <h1>All Trains</h1>
-      {trains.map((train) => (
-        <TrainCard key={train.trainNumber} train={train} />
-      ))}
-    </div>
+    <form onSubmit={handleSubmit}>
+      <label>
+        Company Name:
+        <input
+          type="text"
+          name="companyName"
+          value={formData.companyName}
+          onChange={handleChange}
+        />
+      </label>
+      <br />
+      <label>
+        Owner Name:
+        <input
+          type="text"
+          name="ownerName"
+          value={formData.ownerName}
+          onChange={handleChange}
+        />
+      </label>
+      <br />
+      <label>
+        Roll No:
+        <input
+          type="text"
+          name="rollNo"
+          value={formData.rollNo}
+          onChange={handleChange}
+        />
+      </label>
+      <br />
+      <label>
+        Owner Email:
+        <input
+          type="email"
+          name="ownerEmail"
+          value={formData.ownerEmail}
+          onChange={handleChange}
+        />
+      </label>
+      <br />
+      <label>
+        Access Code:
+        <input
+          type="text"
+          name="accessCode"
+          value={formData.accessCode}
+          onChange={handleChange}
+        />
+      </label>
+      <br />
+      <button type="submit">Submit</button>
+    </form>
   );
 };
 
-export default AllTrainsPage;
+export default TrainForm;
+
+// {
+//   "companyName": "Train Central",
+//   "ownerName": "Ram",
+//   "rollNo": "10",
+//   "ownerEmail": "ram@abc.edu",
+//   "accessCode": "qxrwbC"
+// }
